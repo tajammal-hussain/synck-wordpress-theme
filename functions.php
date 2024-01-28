08a1141f0dad3cbafca3671074d2df69c0d3b8c8
tajammal-hussain <tajammalmajeed@live.com>
2024-01-29 03:43:44 +0500

    Front Page Configured

--- functions.php
+++ functions.php
@@ -51,7 +51,6 @@ add_action('acf/init', function() {
     $page_templates->menu_order = 99;
     
     $page_templates->addToTemplate();
-    $page_templates->addLocation('front_page', 'page_type', '!=', false);
 //    $page_templates->addLocation('posts_page', 'page_type', '!=', false);
     
     // $page_templates->addToFrontpage();
@@ -107,47 +106,56 @@ function mnk_init_frontpage_fields() {
     
     $banner->instructions = 'This field group controls the full height banner displayed at the top of the page. Currently only the text and popout video are controllable, the background video is hard-coded.';
     
-    $banner_text = new Monk\ACF\Field\Textarea('Text');
-    $banner_text->wrapNewLines('br');
-    $banner_text->instructions = "The text to display over the background video.";
+    $banner_text = new Monk\ACF\Field\Wysiwyg('content');
+    $banner_text->instructions = "The text to display Left of the Screen";
     
-    $banner_video = new \Monk\ACF\Field\Basic('Video', 'url');
-    $banner_video->instructions = 'The video to play in the popout display. Can be either a vimeo or youtube link but vimeo is recommended.';
+    $learn_more = new \Monk\ACF\Field\Basic('LearnMore');
+    $learn_more->instructions = 'Learn More Button Please Add Learn more Text';
     
+    $letTalk = new \Monk\ACF\Field\Basic('LetsTalk');
+    $letTalk->instructions = 'Lets Talk Button Please Add Learn more Text';
+
+    $image = new \Monk\ACF\Field\Image('image');
+    $image->instructions = 'Image to the right';
+
+    $experience = new \Monk\ACF\Field\Group('Experience');
+    $image = new \Monk\ACF\Field\Image('experienceImage');
+    $image->instructions = 'Image to the left of the Experience';
+    $number = new \Monk\ACF\Field\Number('experienceNumber');
+    $exp_content = new Monk\ACF\Field\Wysiwyg('experienceContent');
+
+    $experience->addSubField(
+        $image,
+        $number,
+        $exp_content
+    );
+
+
+    $experts = new \Monk\ACF\Field\Group('Experts');
+
+    $repeat_image = new \Monk\ACF\Field\Repeater('image');
+    $ExpertImage = new \Monk\ACF\Field\Image('expertImage');
+    $repeat_image->addSubField(
+        $ExpertImage
+    );
+
+    $expertContent = new Monk\ACF\Field\Wysiwyg('expertcontent');
+    $experts->addSubField(
+        $repeat_image,
+        $expertContent,
+    );
+
+
     $banner->addSubField(
-//            new Monk\ACF\Field('Background', 'oembed'),
-            $banner_text,
-            $banner_video
-        );
+        $banner_text,
+        $learn_more,
+        $letTalk,
+        $image,
+        $experience,
+        $experts
+    );
     
-    $about = new \Monk\ACF\Field\Group('About');
-    $about->instructions = 'This group controls the text that follows immediately after scrolling past the top banner and just before the parallax section.';
-    
-    $about_heading = new \Monk\ACF\Field\Textarea('Heading');
-    $about_heading->instructions = 'This field controls the text that is displayed behind the Content field.';
-    
-    $about_content = new Monk\ACF\Field\Textarea('Content');
-    
-    $about->addSubField(
-            $about_heading->wrapNewLines('br'),
-            $about_content->wrapNewLines(),
-            new \Monk\ACF\Field\Link('Read More')
-        );
-    
-    
-    $quote = new \Monk\ACF\Field\Group('Quote');
-    
-    $quote_text     = new Monk\ACF\Field\Textarea('Quote');
-    $quote_cite     = new Monk\ACF\Field\Textarea('Cite');
-    $quote_video    = new Monk\ACF\Field\Basic('Background', 'oembed');
-    
-    $quote->addSubField(
-            $quote_video,
-            $quote_text->wrapNewLines(),
-            $quote_cite->wrapNewLines('br')
-        );
-    
-    $frontpage->addField($notes, $banner, $about, $quote);
+    $frontpage->addField($banner);
     
     return $frontpage;
     
