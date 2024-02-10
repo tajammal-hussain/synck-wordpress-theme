4e3d98bf8518cdc5ade8a41a89de9c4c58298099
Tajammal Hussain <tajammalmajeed@live.com>
2024-02-10 15:04:53 +0500

    Fixes

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * The blog template file.
+     *
+     * @package          Synck\Templates\Page
+     * @Synck-version 1.0.0
+     */
+    get_header();
+    do_action( 'synck_before_page' );
+?>
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php do_action( 'synck_before_page_content' ); ?>
+        
+            <?php the_content(); ?>
+
+            <?php if ( comments_open() || '0' != get_comments_number() ){
+                comments_template(); } ?>
+
+        <?php do_action( 'synck_after_page_content' ); ?>
+    <?php endwhile; // end of the loop. ?>
+
+<?php
+    do_action( 'synck_after_page' );
+    get_footer();
+
+?>
